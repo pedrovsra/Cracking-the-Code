a9a2ae7343d0d4441b706d12d3b688574811990b
pedrovsra <ptvsr@cin.ufpe.br>
2015-04-29 21:59:55 -0300

    Update 9-4.java

--- 9-4.java
+++ 9-4.java
@@ -19,3 +19,25 @@ public static ArrayList<ArrayList<Integer>> getSubSets(int at, ArrayList<Integer
 	}
 	return allSubSets;
 }
+
+public static ArrayList<ArrayList<Integer>> getSubSets(ArrayList<Integer> set) {
+	ArrayList<ArrayList<Integer>> allSubSets = new ArrayList<ArrayList<Integer>>();
+
+	int max = 1 << set.size();
+	for(int k = 0; k < max; k++) {
+		ArrayList<Integer> subset = convertIntToSet(k, set);
+		allSubSets.add(subset);
+	}
+	return allSubSets;
+}
+
+private static ArrayList<Integer> convertIntToSet(int x, ArrayList<Integer> set) {
+	ArrayList<Integer> subset = new ArrayList<Integer>();
+	int index = 0;
+
+	for(int i = x; i > 0; x >>= 1) {
+		if((x & 1) == 1) subset.add(set.get(index));
+		index++;
+	}
+	return subset;
+}
